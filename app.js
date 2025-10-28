918fd8afc8930ab5f79fe6706597bc1b1d8b4571
Usman908665 <websitedevelopment190@gmail.com>
2025-10-28 07:07:53 -0700

    I have uploaded my all assignments
    I have added my assignment of JS

---
+++ app.js
@@ -0,0 +1,24 @@
+//  var imgEL = document.getElementById('apple');
+//  var imgContainer = document.getElementById('i-section');
+//  function changeBG(){
+//     imgEL.src = "images/mango.jpeg";
+//  }
+
+//Assignment # 2
+
+// var currentSize = 48;
+
+// // function increaseFontSize(){
+// //     currentSize += 6;
+// //     document.getElementById('fontName').style.fontSize = currentSize + 'px';
+// //     console.log(currentSize);
+// // }
+
+//Assignment # 3
+
+var para = document.getElementById('para');
+var hiddenArea = document.getElementById('hidden');
+
+function showText() {
+    hiddenArea.style.visibility = 'visible';
+}
